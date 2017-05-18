9afac29691f248e3e70b3d1aad91da8afe6feb8c
Chad Shryock <chad@keystone.works>
2017-05-18 15:34:06 -0400

    Make installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+from rszio import version
+
+setup(
+    name='django-rszio',
+    author='Chad Shryock',
+    author_email='chad@keystone.works',
+    version=version,
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/g3rd/django-rszio',
+    zip_safe=True,
+    install_requires=[
+        'django>=1.10',
+        'requests>=2.14.2',
+    ],
+)
